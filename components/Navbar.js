1b37558270f36fde2c2fa0840efc23c39af92e15
Sean Pettit <s.p@mac.com>
2020-03-14 16:15:13 +0000

    more clean up

--- components/Navbar.js
+++ components/Navbar.js
@@ -14,27 +14,27 @@ import {
 } from "@blueprintjs/core";
 
 const NavbarComponent = ({ router: { pathname } }) => (
-  <nav class="bp3-navbar bp3-dark">
+  <nav className="bp3-navbar bp3-dark">
     {/* <div style={{ margin: "0 auto", width: "480px" }}> */}
-    <div class="bp3-navbar-group bp3-align-left">
-      <div class="bp3-navbar-heading">Travels</div>
+    <div className="bp3-navbar-group bp3-align-left">
+      <div className="bp3-navbar-heading">Travels</div>
     </div>
-    <div class="bp3-navbar-group bp3-align-right">
+    <div className="bp3-navbar-group bp3-align-right">
       <Link href="/">
         <a className={pathname === "/" ? "is-active" : ""}>
-          <button class="bp3-button bp3-minimal bp3-icon-home">Home</button>
+          <button className="bp3-button bp3-minimal bp3-icon-home">Home</button>
         </a>
       </Link>
       <Link href="/client-only">
         <a className={pathname === "/client-only" ? "is-active" : ""}>
-          <button class="bp3-button bp3-minimal bp3-icon-user">
+          <button className="bp3-button bp3-minimal bp3-icon-user">
             Client Only
           </button>
         </a>
       </Link>
       <Link href="/about">
         <a className={pathname === "/about" ? "is-active" : ""}>
-          <button class="bp3-button bp3-minimal bp3-icon-cog"></button>
+          <button className="bp3-button bp3-minimal bp3-icon-cog"></button>
         </a>
       </Link>
     </div>
